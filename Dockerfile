8473c4f5269537fb04b2853be7f92e23cae10f08
Wasiul Islam Sakif <sakif049@gmail.com>
2024-11-13 08:47:35 +0600

    fix: php version issue

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,2 @@
-FROM php:7.0-apache
-
-COPY ./src .
-
+FROM php:8.2.25-apache
 RUN docker-php-ext-install mysqli 
--- db-init/init.sql
+++ db-init/init.sql
@@ -7,6 +7,8 @@
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
+ALTER USER ‘user’@’localhost’ IDENTIFIED WITH mysql_native_password BY ‘password’;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
